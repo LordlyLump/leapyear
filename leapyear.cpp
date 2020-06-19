a03db1d6072d17c16f70fef634d385ce2ed9127e
Sebastian Ricks Pedersen <Sebaricks@gmail.com>
2020-06-19 22:20:58 +0200

    First attemot at building is_leap_year

--- leapyear.cpp
+++ leapyear.cpp
@@ -2,10 +2,22 @@
 
 bool is_leap_year(int year)
 {
+
+const int four{4}, hundred{100}, fourhundred{400};
+    
+   if (((year / four) && (year / hundred == false)) && year / fourhundred ) {
+
+   
+   return true;
+   }
+   
     // complete the code for this function, so that
     // it returns true for leap years (divisible by 4 or 400)
     // and false for years that are not divisible by 4, or are also divisible by 100 but not by 400
+    else {
+    
     return false;
+    }
 }
 
 int main() {
